90af4088678a0f55847957d3e5ee03f6d770e283
israel1996 <mx_2013@outlook.com>
2023-01-24 17:08:49 -0500

    Codificación en Main Activity

--- app/src/main/java/com/example/snake_speech/MainActivity.java
+++ app/src/main/java/com/example/snake_speech/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.snake_speech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
+    private SurfaceView surfaceView;
+    private TextView scoreTV;
+    //Superficie para dibujar la snake en canva
+    private SurfaceHolder surfaceHolder;
+    private String movingPosition = "right";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        scoreTV = findViewById(R.id.scoreTV);
+
+        final AppCompatImageButton topBtn = findViewById(R.id.topBtn);
+        final AppCompatImageButton leftBtn = findViewById(R.id.leftBtn);
+        final AppCompatImageButton rightBtn = findViewById(R.id.rightBtn);
+        final AppCompatImageButton bottomBtn = findViewById(R.id.bottomBtn);
+
+        //adding callback to surfaceview
+        surfaceView.getHolder().addCallback(this);
+
+        topBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!movingPosition.equals("bottom")){
+                    movingPosition = "top";
+                }
+            }
+        });
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!movingPosition.equals("right")){
+                    movingPosition = "left";
+                }
+            }
+        });
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!movingPosition.equals("left")){
+                    movingPosition = "right";
+                }
+            }
+        });
+        bottomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!movingPosition.equals("top")){
+                    movingPosition = "bottom";
+                }
+            }
+        });
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+        init();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    private void init(){
+
     }
 }
