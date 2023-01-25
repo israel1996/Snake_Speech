76c89d39bfd37b573681849f6fd9baaa79cc8f62
israel1996 <mx_2013@outlook.com>
2023-01-24 23:43:12 -0500

    Codificación de Snake Game

--- app/src/main/java/com/example/snake_speech/MainActivity.java
+++ app/src/main/java/com/example/snake_speech/MainActivity.java
@@ -1,22 +1,53 @@
 package com.example.snake_speech;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
 
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
+    private final List<SnakePoints> snakePointsList = new ArrayList<>();
     private SurfaceView surfaceView;
     private TextView scoreTV;
     //Superficie para dibujar la snake en canva
     private SurfaceHolder surfaceHolder;
     private String movingPosition = "right";
+
+    private int score = 0;
+
+
+    private static final int pointSize = 28;
+    private static final int defaultTalePoints = 3;
+    private static final int snakeColor = Color.YELLOW;
+    private static final int snakeMovingSpeed = 800;
+    private int positionX, positionY;
+
+    private Timer timer;
+
+    private Canvas canvas = null;
+    private Paint pointColor = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,5 +116,154 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     private void init(){
 
+        snakePointsList.clear();
+
+        scoreTV.setText("0");
+
+        score = 0;
+
+        movingPosition = "right";
+
+        int startPositionX = (pointSize) * defaultTalePoints;
+
+        for(int i=0; i < defaultTalePoints; i++){
+            SnakePoints snakePoints = new SnakePoints(startPositionX, pointSize);
+            snakePointsList.add(snakePoints);
+
+            startPositionX = startPositionX - (pointSize * 2);
+        }
+
+        addPoint();
+        moveSnake();
+    }
+    private void addPoint(){
+        int surfaceWidht = surfaceView.getWidth() - (pointSize * 2);
+        int surfaceHeight = surfaceView.getHeight() - (pointSize * 2);
+
+        int randomXPosition = new Random().nextInt(surfaceWidht / pointSize);
+        int randomYPosition = new Random().nextInt(surfaceHeight / pointSize);
+
+        if((randomXPosition % 2) != 0){
+            randomXPosition = randomXPosition + 1;
+        }
+        if((randomYPosition % 2) != 0){
+            randomYPosition = randomYPosition + 1;
+        }
+        positionX = (pointSize * randomXPosition) + pointSize;
+        positionY = (pointSize * randomYPosition) + pointSize;
+    }
+    private void moveSnake(){
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                int headPositionX = snakePointsList.get(0).getPositionX();
+                int headPositionY = snakePointsList.get(0).getPositionY();
+
+                if(headPositionX == positionX && positionY == headPositionY){
+                    groweSnake();
+                    addPoint();
+                }
+                switch (movingPosition){
+                    case "right":
+                        snakePointsList.get(0).setPositionX(headPositionX + (pointSize * 2));
+                        snakePointsList.get(0).setPositionY(headPositionY);
+                        break;
+                    case "left":
+                        snakePointsList.get(0).setPositionX(headPositionX - (pointSize * 2));
+                        snakePointsList.get(0).setPositionY(headPositionY);
+                        break;
+                    case "top":
+                        snakePointsList.get(0).setPositionX(headPositionX);
+                        snakePointsList.get(0).setPositionY(headPositionY - (pointSize * 2));
+                        break;
+                    case "bottom":
+                        snakePointsList.get(0).setPositionX(headPositionX);
+                        snakePointsList.get(0).setPositionY(headPositionY + (pointSize * 2));
+                        break;
+
+                }
+                if(checkGameOver(headPositionX, headPositionY)){
+                    timer.purge();
+                    timer.cancel();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Your Score = "+score);
+                    builder.setTitle("Game Over");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Start Again", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            init();
+                        }
+                    });
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            builder.show();
+                        }
+                    });
+                }
+                else{
+                    canvas = surfaceHolder.lockCanvas();
+                    canvas.drawColor(Color.WHITE, PorterDuff.Mode.CLEAR);
+                    canvas.drawCircle(snakePointsList.get(0).getPositionX(), snakePointsList.get(0).getPositionY(), pointSize, createPointColor());
+                    canvas.drawCircle(positionX, positionY, pointSize, createPointColor());
+
+                    for(int i = 1; i < snakePointsList.size(); i++){
+                        int getTempPositionX = snakePointsList.get(i).getPositionX();
+                        int getTempPositionY = snakePointsList.get(i).getPositionY();
+
+                        snakePointsList.get(i).setPositionX(headPositionX);
+                        snakePointsList.get(i).setPositionY(headPositionY);
+                        canvas.drawCircle(snakePointsList.get(i).getPositionX(), snakePointsList.get(i).getPositionY(), pointSize, createPointColor());
+
+                        headPositionX = getTempPositionX;
+                        headPositionY = getTempPositionY;
+
+                    }
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }, 1000 - snakeMovingSpeed, 1000 - snakeMovingSpeed);
+    }
+
+    private void groweSnake(){
+        SnakePoints snakePoints = new SnakePoints(0, 0);
+        snakePointsList.add(snakePoints);
+        score++;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                scoreTV.setText(String.valueOf(score));
+            }
+        });
+    }
+    private boolean checkGameOver(int headPositionX, int headPositionY){
+        boolean gameOver = false;
+        if(snakePointsList.get(0).getPositionX() < 0 ||
+                snakePointsList.get(0).getPositionY() < 0 ||
+                snakePointsList.get(0).getPositionX() >= surfaceView.getWidth() ||
+                snakePointsList.get(0).getPositionY() >= surfaceView.getHeight()
+        ){
+            gameOver = true;
+        }else{
+            for(int i = 1; i < snakePointsList.size(); i++){
+                if(headPositionX == snakePointsList.get(i).getPositionX() &&
+                headPositionY == snakePointsList.get(i).getPositionY()){
+                    gameOver = true;
+                    break;
+                }
+            }
+        }
+        return gameOver;
+    }
+    private Paint createPointColor(){
+        if(pointColor == null){
+            pointColor = new Paint();
+            pointColor.setColor(snakeColor);
+            pointColor.setStyle(Paint.Style.FILL);
+            pointColor.setAntiAlias(true);
+        }
+        return pointColor;
     }
 }
